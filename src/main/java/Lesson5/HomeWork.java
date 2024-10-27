ac3e655d81fd48cdada1202f58c9360697e15a41
sanjar-qosim <sanjar171766@gmail.com>
2024-10-27 22:26:11 +0500

    Added home class

--- src/main/java/Lesson5/HomeWork.java
+++ src/main/java/Lesson5/HomeWork.java
@@ -3,5 +3,6 @@ package Lesson5;
 public class HomeWork {
     public static void main(String[] args) {
         System.out.println("This file will be used for Home task №5");
+        System.out.println("This file will be used for Home task №5");
     }
 }
