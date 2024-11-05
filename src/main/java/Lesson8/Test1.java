6f586ec82bbea5eec0b4fb7ac1f3f93fccd06e80
sanjar-qosim <sanjar171766@gmail.com>
2024-11-06 00:59:44 +0500

    Start 8th lesson

---
+++ src/main/java/Lesson8/Test1.java
@@ -0,0 +1,11 @@
+package Lesson8;
+
+public class Test1 {
+    public final int a = 10;
+
+    public static void main(String[] args) {
+        Test1 t1 = new Test1();
+//        t1.a = t1.a * 2;
+        System.out.println(t1.a);
+    }
+}
