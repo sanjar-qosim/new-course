36b8ca0f13e5ce58fb6ec6c0d992cf2fa59b250e
sanjar-qosim <sanjar171766@gmail.com>
2024-11-19 22:19:16 +0500

    Finish 14th Homework

--- src/main/java/Lesson14/HomeWork.java
+++ src/main/java/Lesson14/HomeWork.java
@@ -1,20 +1,54 @@
 package Lesson14;
 
 public class HomeWork {
-    static void time(){
-        HOUR: for (int hour = 0; hour <= 5; hour++) {
-            MINUTES: for (int minute = 0; minute <= 59; minute++) {
-            if (hour <= 1 && minute == 10) {
-                break HOUR;
-            }
-                SECOND: for (int seconds = 0; seconds <= 59; seconds++) {
+    static void time() {
+        for (int hour = 0; hour <= 5; hour++) {
+            for (int minute = 0; minute <= 59; minute++) {
+                if (hour > 1 && minute % 10 == 0) {
+                    return;
+                }
+                for (int seconds = 0; seconds <= 59; seconds++) {
                     System.out.println(hour + ":" + minute + ":" + seconds);
                 }
             }
         }
     }
 
+    static void task3(int a){
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a + " * " + i + " = " + a*i);
+        }
+    }
+
+    static void task4(int a){
+        int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+
+    static void task5(){
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    static void task6(int a){
+        int fact = 1;
+        for (int i = 1; i <= a; i++) {
+            fact *= i;
+        }
+        System.out.println(fact);
+    }
+
     public static void main(String[] args) {
-        time();
+//        time();
+//        task3(3);
+//        task4(3);
+//        task5();
+//        task6(5);
     }
 }
