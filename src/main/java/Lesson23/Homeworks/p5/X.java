beee7f7f43133c988f76d1e06a1565f70f712b7c
sanjar-qosim <sanjar171766@gmail.com>
2024-12-24 01:39:46 +0500

    Homework 23rd lesson

---
+++ src/main/java/Lesson23/Homeworks/p5/X.java
@@ -0,0 +1,32 @@
+package Lesson23.Homeworks.p5;
+
+public class X {
+
+    public X(){
+        System.out.println("X");
+    }
+
+    public X(int i){
+        System.out.println("X" + i);
+    }
+
+    private boolean abc(){
+        return false;
+    }
+
+    public static void main(String[] args) {
+        X x = new Y(18);
+        System.out.println(x.abc());
+    }
+}
+
+class Y extends X {
+
+    public Y(int i){
+        System.out.println("Y");
+    }
+
+    public boolean abc(){
+        return true;
+    }
+}
