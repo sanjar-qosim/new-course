97c85da4d60b1fce413874462eaf254b1c68f5b1
sanjar-qosim <sanjar171766@gmail.com>
2024-12-31 00:45:22 +0500

    Finish 24th lesson

--- src/main/java/Lesson24/Test5.java
+++ src/main/java/Lesson24/Test5.java
@@ -4,23 +4,31 @@ public class Test5 {
 }
 
 interface I1 {
-    void abc();
+    static void def(){
+        System.out.println("This is a static method");
+    }
 
-    default void def(){
-        System.out.println("This is the def method");
+    default void abc(){
+        System.out.println("This is the abc method");
     };
 }
 
-class Z2 implements I1 {
-
-    @Override
-    public void abc(){
-        System.out.println("This is the abc method");
-    }
-
+class R implements I1 {
     public static void main(String[] args) {
-        Z2 z2 = new Z2();
-        z2.abc();
-        z2.def();
+        I1.def();
     }
 }
+
+//class Z2 implements I1 {
+//
+//    @Override
+//    public void abc(){
+//        System.out.println("This is the abc method");
+//    }
+//
+//    public static void main(String[] args) {
+//        Z2 z2 = new Z2();
+//        z2.abc();
+//        z2.def();
+//    }
+//}
