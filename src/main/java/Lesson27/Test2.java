891a913687053dec8dd9ffcd5db94d1b0f7463f4
sanjar-qosim <sanjar171766@gmail.com>
2025-01-29 00:20:20 +0500

    Continue 27th lesson

---
+++ src/main/java/Lesson27/Test2.java
@@ -0,0 +1,13 @@
+package Lesson27;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        int arr [] = {1, 2, 0};
+        System.out.println(arr[5]);
+        System.out.println("Hello");
+
+        String s = null;
+        System.out.println(s.length());
+    }
+}
---
+++ src/main/java/Lesson27/Test3.java
@@ -0,0 +1,11 @@
+package Lesson27;
+
+import java.io.*;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        File f = new File("test10.txt");
+
+    }
+}
