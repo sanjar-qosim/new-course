aa44af5cb27ea12b937dec0e166bcc1a4683593b
sanjar-qosim <sanjar171766@gmail.com>
2025-01-30 01:26:56 +0500

    Continue 27th lesson

--- src/main/java/Lesson27/Test3.java
+++ src/main/java/Lesson27/Test3.java
@@ -4,8 +4,12 @@ import java.io.*;
 
 public class Test3 {
 
-    public static void main(String[] args) {
-        File f = new File("test10.txt");
+    public static void main(String[] args) throws Exception {
+        File f = new File("test11.txt");
+        FileInputStream fis = new FileInputStream(f);
+        fis.read();
 
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(100);
     }
 }
