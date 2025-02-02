7e081b4668b09d8eb5a1d55997ec40335a746c10
sanjar-qosim <sanjar171766@gmail.com>
2025-02-02 11:44:35 +0500

    Continue 27th lesson

--- src/main/java/Lesson27/Test4.java
+++ src/main/java/Lesson27/Test4.java
@@ -8,9 +8,12 @@ public class Test4 {
         File f =new File("test.txt");
 
         try {
+            FileInputStream fis = new FileInputStream(f);
             System.out.println("Bye");
+        } catch (FileNotFoundException e) {
+            System.out.println("Was caught exception with file: " + e);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Was caught exception");
+            System.out.println("Was caught exception with array");
         } finally {
             System.out.println("This is finally block");
         }
